4bcb825063ab800ce1ac7ba3f5724dd0f260078c
SergeyBel <belovseregas@gmail.com>
2016-01-28 15:58:00 +0300

    Add source files

---
+++ gost341112.h
@@ -0,0 +1,31 @@
+#ifndef _GOST341112_H_
+#define _GOST341112_H_
+
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+
+class Streebog
+{
+    private:
+        int mode;
+        void precalc_mul_table();
+        void lps (unsigned char *in, unsigned long long *out);
+        void ToHex(long long n, unsigned long long *c);
+        void Xor64(unsigned long long *a,unsigned long long *b,unsigned long long *c);
+        void Sum(unsigned char*a, unsigned char *b, unsigned char *c);
+        void E(unsigned long long *k, unsigned long long *m);
+        void g(unsigned char *h, unsigned char *m, unsigned long long *n);
+
+    public:
+        Streebog(int mode = 512);
+        unsigned char *Hash(unsigned char *M, unsigned int size);
+        int GetMode();
+		void SetMode(int mode);
+};
+
+
+
+
+#endif
